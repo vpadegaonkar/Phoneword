64ef718d7d7b74e66121f013a60a8ef8c0b6628f
vpadegaonkar <vpadegaonkar@yahoo.com>
2018-11-24 09:28:02 +0530

    Add project files.

---
+++ Phoneword/Phoneword/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Phoneword
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            AppCenter.Start("android=013101a5-e7b1-4b25-8783-1b0a32cb1a91;", typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
